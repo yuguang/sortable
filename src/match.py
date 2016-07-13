2433c0480397b222d6f3a8cc5eea718a2bcb1064
Yuguang Zhang <zh.yuguang.cn@gmail.com>
2016-07-13 16:41:10 -0400

    save matches to results file

--- src/match.py
+++ src/match.py
@@ -20,6 +20,11 @@ from pyspark.sql import SQLContext
 4. Collect together listings under the same products
 '''
 
+def debug(obj):
+    # convenience function for debug output
+    if DEBUG:
+        pprint(obj)
+
 def canonical_format(df, Item):
     # 1. map to name, id, and manufacturer dictionary
     # 2. map to row number, line tuples
@@ -71,37 +76,48 @@ if __name__ == "__main__":
     listings_training_dict = json.load(open(LISTINGS_TRAINING_PATH))
     # train model
     gazetteer.sample(products_dict, listings_dict, 10000)
-    training_pairs = trainingDataLink(products_training_dict, listings_training_dict, 'labelled_id', 500)
+    training_pairs = trainingDataLink(products_training_dict, listings_training_dict, 'labelled_id', 10)
     gazetteer.markPairs(training_pairs)
     gazetteer.train()
 
-    # partition listings and match with products
+    # add products to the index of records to match against
     if not gazetteer.blocked_records:
-        gazetteer.index(listings_dict)
+        gazetteer.index(products_dict)
 
-    alpha = gazetteer.threshold(products_dict, recall_weight=.5)
+    alpha = gazetteer.threshold(listings_dict, recall_weight=.5)
 
 
     # identify records that all refer to the same entity
     print('clustering...')
-    clustered_dupes = gazetteer.match(products_dict, threshold=alpha, n_matches=1)
+    clustered_dupes = gazetteer.match(listings_dict, threshold=alpha)
+    debug(clustered_dupes)
 
     match_dict = defaultdict(list)
     # fill in null entries
     listings_df.fillna('')
+    pairs = []
     # convert to data frame
     for matches in clustered_dupes:
-        # loop through a sequence of record ids and corresponding sequence of confidence score
         for pair, score in matches:
-            product = products_df.filter(products_df['id'] == int(pair[0])).first()
-            listing = listings_df.filter(listings_df['id'] == int(pair[1])).first()
-            match_dict[product.product_name].append({
-                "title": listing.title,
-                "manufacturer": listing.manufacturer,
-                "currency": listing.currency,
-                "price": listing.price
-            })
-    pprint(match_dict)
+            pairs.append((int(pair[0]), int(pair[1])))
+    matches_df = sqlContext.createDataFrame(pairs, ['listing_id', 'product_id'])
+    # get product and listing information for each matched pair
+    matches_df = matches_df.join(products_df.select('id', 'product_name'), products_df.id == matches_df.product_id).join(listings_df.select('id', 'title', 'manufacturer', 'currency', 'price'), listings_df.id == matches_df.listing_id)
+    # collect listings into lists with matched products
+    for match in matches_df.collect():
+        match_dict[match.product_name].append({
+            "title": match.title,
+            "manufacturer": match.manufacturer,
+            "currency": match.currency,
+            "price": match.price
+        })
+    debug(match_dict)
 
-
+    with open(OUTPUT_PATH, 'w') as output_file:
+        for product_name, listings in match_dict.items():
+            result = {
+                "product_name": product_name,
+                "listings": listings
+            }
+            output_file.write('{}\n'.format(json.dumps(result)))
 
